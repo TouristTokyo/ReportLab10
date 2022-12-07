9728f9a73849d2ba1e2649aa7abc0b9b5d49092a
Mishka <Fiz-mat228@yandex.ru>
2022-12-07 20:01:59 +0300

    first commit

---
+++ Faculty.java
@@ -0,0 +1,32 @@
+public class Faculty {
+
+	public Faculty(Employee dean, String name) {
+		this.dean = dean;
+		this.name = name;
+	}
+
+	private Employee dean;
+	private String name;
+
+	public Employee getDean() {
+		return dean;
+	}
+
+	public void setDean(Employee dean) {
+		this.dean = dean;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCountLecturers(){
+		return 100;
+	}
+
+
+}
